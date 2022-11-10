ea36652eb317e2e15e84fec511abee128213be8b
kakxem <80788785+kakxem@users.noreply.github.com>
2022-11-10 20:13:04 +0100

    fix: require path fixed

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,7 +1,7 @@
 import type { PipewireLink, PipewirePort, PipewireNode } from "./types";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const library = require("./../../build/index.node");
+const library = require("./index.node");
 
 export function createPwThread() {
   return library.createPwThread();
